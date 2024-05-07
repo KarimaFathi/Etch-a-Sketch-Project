90e29989795375362dbb78ff21ca686b3a80afac
karimaFathi <karimafathifadili@gmail.com>
2024-05-07 22:19:20 +0100

    set up hover effect for grid divs

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const grid = document.createElement("div");
-grid.className = "grid";
+grid.className = "gridClone";
 const container = document.querySelector(".container");
 
 
@@ -8,4 +8,11 @@ for(let  i=0;i<256;i++){
    grid.style.height = `${600 / 16}px`;
    container.appendChild(grid.cloneNode());
 }
+
+const grids = document.querySelectorAll('.gridClone');
+grids.forEach((clonedNode)=>{
+   clonedNode.addEventListener('mouseenter', (e)=> {
+      e.target.style.backgroundColor = 'black';
+   })
+});
 
